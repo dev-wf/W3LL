ab145bcb434d06a6d434d9814d5b195e25775da5
Welington Fernandes <w.a.f@outlook.com.br>
2025-03-13 23:15:42 -0300

    DELETADO

--- OLD/js/index.js
+++
@@ -1,7 +0,0 @@
-console.log('index.js carregado no projeto...');
-// criando excesáo de largura de tela
-const tam = document.body.clientWidth;
-console.log('Largura do body:', tam);
-if (tam < 400) {
-    alert('Tela muito pequena para o projeto...');
-} 
